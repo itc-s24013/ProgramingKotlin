91af08a5399a8b2af42bd57a0d338ab65ef3a19e
itc-s24013 <s24013@std.it-college.ac.jp>
2025-05-28 15:34:15 +0900

    2025/05/28までの内容28章(メソッドのオーバーライド)まで

---
+++ src/chap28/Ferrari.kt
@@ -0,0 +1,10 @@
+package chap28
+
+class Ferrari(color: String) : Car(color) {
+    override fun drive(d: Double) { //表面上では変わったように見えるが内部的には変わらないため上書きとは少し違う
+        distance += d
+        println("${color}のフェラーリが走っています！ブウォォォーン！")
+        println("おおっ、かっこいい！！！")
+        println("マイレージは ${distance}キロになったぜい！")
+    }
+}
---
+++ src/chap28/code28_6.kt
@@ -0,0 +1,6 @@
+package chap28
+
+fun main() {
+    val prius = Prius("グレー")
+    prius.drive(500.0)
+}
